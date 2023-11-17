17665029a72f1384e330183410ad7f50bb8b5b34
Hazem Magdy <hhazem0010@gmail.com>
2023-11-17 14:10:19 -0800

    console & web_static, task 4

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to represent Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class to represent the Amenity
+
+    Attributes:
+        name (str): Amenity name.
+    """
+
+    name = ""
